2001c10be61fe272ecb78b151926e515b1ca06f4
Francisco Castillo <fcastillog@ripley.com>
2020-08-10 20:50:59 -0400

    FEATURE - init folder components, constants, services, utils, validations, views

---
+++ src/services/settings.js
@@ -0,0 +1,29 @@
+import buildUrl from 'build-url'
+
+export default {
+  urls: {
+    apiUrl: process.env.REACT_APP_API_URL,
+  },
+  path: {
+    listCounters: '/api/v1/counter',
+    addCounter: '/api/v1/counter',
+    incrementCounter: '/api/v1/counter/inc',
+    decrementCounter: '/api/v1/counter/dec',
+    deleteCounter: '/api/v1/counter',
+  },
+  buildUrl: (
+    url = null,
+    path = null,
+    queryParams = null,
+    hash = null,
+    lowerCase = false,
+    disableCSV = false,
+  ) =>
+    buildUrl(url, {
+      path,
+      hash,
+      lowerCase,
+      disableCSV,
+      queryParams,
+    }),
+}
