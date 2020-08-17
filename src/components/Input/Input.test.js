2d4e3cbb229cb9b1937b005da22800e308410db2
Francisco Castillo <fcastillog@ripley.com>
2020-08-17 19:35:07 -0400

    FEATURE - add interactive test to inputs and pickers

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { act } from '@testing-library/react-hooks';
+import userEvent from '@testing-library/user-event';
 import Input from './Input';
 import SearchInput from './SearchInput';
 
 describe('404', () => {
-  it('renders Input', async () => {
+  it('Input renders OK with value', async () => {
     render(<Input value="hello world" setValue={() => {}} />);
     const inputTest = screen.getByRole('textbox');
     expect(inputTest.value).toBe('hello world');
   });
+  it('Input writes OK', async () => {
+    let value = '';
+    render(
+      <Input
+        value={value}
+        setValue={(e) => {
+          value += e;
+        }}
+      />
+    );
+    const inputTest = screen.getByRole('textbox');
+    await act(() => userEvent.type(inputTest, 'TEST'));
+    expect(value).toBe('TEST');
+  });
+
   it('renders SearchInput', async () => {
     render(
       <SearchInput
@@ -20,5 +37,38 @@ describe('404', () => {
     );
     const searchInputTest = screen.getByRole('textbox');
     expect(searchInputTest.value).toBe('hello world');
+  });
+  it('SearchInput searches OK', async () => {
+    let value = '';
+    let searched = [];
+    const data = [
+      { title: 'Hot-dogs', display: true },
+      { title: 'Cupcakes eaten', display: true },
+      { title: 'Chicken food', display: true },
+      { title: 'Dog food', display: true },
+      { title: 'What?', display: true },
+    ];
+    const expectedResult = [
+      { title: 'Hot-dogs', display: false },
+      { title: 'Cupcakes eaten', display: false },
+      { title: 'Chicken food', display: false },
+      { title: 'Dog food', display: false },
+      { title: 'What?', display: true },
+    ];
+    render(
+      <SearchInput
+        data={data}
+        onSearch={(e) => {
+          searched = e;
+        }}
+        value={value}
+        setValue={(e) => {
+          value += e;
+        }}
+      />
+    );
+    const inputTest = screen.getByRole('textbox');
+    await act(() => userEvent.type(inputTest, 'What?'));
+    expect(JSON.stringify(expectedResult)).toBe(JSON.stringify(searched));
   });
 });
--- src/components/Picker/Picker.test.js
+++ src/components/Picker/Picker.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { act } from '@testing-library/react-hooks';
+import userEvent from '@testing-library/user-event';
 import { BadgePicker, Picker } from './index';
 import { EXAMPLES } from '../../constants/examples';
 
@@ -15,6 +17,21 @@ describe('Picker', () => {
     screen.getByText(EXAMPLES[0].label);
     screen.getByText(EXAMPLES[0].data[0]);
   });
+  it('BadgePicker Clicks OK', async () => {
+    let picked = {};
+    render(
+      <BadgePicker
+        label={EXAMPLES[0].label}
+        data={EXAMPLES[0].data}
+        onSelect={(e) => {
+          picked = e;
+        }}
+      />
+    );
+    const inputTest = screen.getByText(EXAMPLES[0].data[0]);
+    act(() => userEvent.click(inputTest));
+    expect(picked).toBe(EXAMPLES[0].data[0]);
+  });
   it('renders Picker with props', async () => {
     const testText = 'test text';
     render(
@@ -26,5 +43,58 @@ describe('Picker', () => {
       />
     );
     screen.getByText(testText);
+  });
+
+  it('Picker buttons inc OK', async () => {
+    const testText = 'test text';
+    let number = 0;
+    render(
+      <Picker
+        label={testText}
+        number={number}
+        onRemove={() => {}}
+        onAdd={() => {
+          number += 1;
+        }}
+      />
+    );
+    screen.getByText(testText);
+    const addTest = screen.getByLabelText('Add');
+    act(() => userEvent.click(addTest));
+    expect(number).toBe(1);
+  });
+  it('Picker buttons dec OK', async () => {
+    const testText = 'test text';
+    let number = 1;
+    render(
+      <Picker
+        label={testText}
+        number={number}
+        onRemove={() => {
+          number += -1;
+        }}
+        onAdd={() => {}}
+      />
+    );
+    const decTest = screen.getByLabelText('Remove');
+    act(() => userEvent.click(decTest));
+    expect(number).toBe(0);
+  });
+  it('Picker buttons dec past 0 OK', async () => {
+    const testText = 'test text';
+    let number = 0;
+    render(
+      <Picker
+        label={testText}
+        number={number}
+        onRemove={() => {
+          number += -1;
+        }}
+        onAdd={() => {}}
+      />
+    );
+    const decTest = screen.getByLabelText('Remove');
+    act(() => userEvent.click(decTest));
+    expect(number).toBe(0);
   });
 });
